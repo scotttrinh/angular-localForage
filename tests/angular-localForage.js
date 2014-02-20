dfd580d396ba27cadb127e4ce5747606b3b9d62b
mrobin <mrobin@companeo.com>
2014-02-20 17:05:50 +0100

    more asserts with a better organization

--- tests/angular-localForage.js
+++ tests/angular-localForage.js
@@ -18,4 +18,74 @@ describe('Module: LocalForageModule', function () {
         expect(myService.bind).toBeDefined();
         expect(myService.unbind).toBeDefined();
     });
+
+    it('service:setDriver should be defined', function () {
+        var $injector = angular.injector(['LocalForageModule']);
+        var myService = $injector.get('$localForage');
+        expect(myService.setDriver).toBeDefined();
+    });
+
+    it('service:getDriver should be defined', function () {
+        var $injector = angular.injector(['LocalForageModule']);
+        var myService = $injector.get('$localForage');
+        expect(myService.getDriver).toBeDefined();
+    });
+
+    it('service:set should be defined', function () {
+        var $injector = angular.injector(['LocalForageModule']);
+        var myService = $injector.get('$localForage');
+        expect(myService.set).toBeDefined();
+    });
+
+    it('service:get should be defined', function () {
+        var $injector = angular.injector(['LocalForageModule']);
+        var myService = $injector.get('$localForage');
+        expect(myService.get).toBeDefined();
+    });
+
+    it('service:remove should be defined', function () {
+        var $injector = angular.injector(['LocalForageModule']);
+        var myService = $injector.get('$localForage');
+        expect(myService.remove).toBeDefined();
+    });
+
+    it('service:clearAll should be defined', function () {
+        var $injector = angular.injector(['LocalForageModule']);
+        var myService = $injector.get('$localForage');
+        expect(myService.clearAll).toBeDefined();
+    });
+
+    it('service:getKeyAt should be defined', function () {
+        var $injector = angular.injector(['LocalForageModule']);
+        var myService = $injector.get('$localForage');
+        expect(myService.getKeyAt).toBeDefined();
+    });
+
+    it('service:getKeys should be defined', function () {
+        var $injector = angular.injector(['LocalForageModule']);
+        var myService = $injector.get('$localForage');
+        expect(myService.getKeys).toBeDefined();
+    });
+
+    it('service:getLength should be defined', function () {
+        var $injector = angular.injector(['LocalForageModule']);
+        var myService = $injector.get('$localForage');
+        expect(myService.getLength).toBeDefined();
+    });
+
+    it('service:bind should be defined', function () {
+        var $injector = angular.injector(['LocalForageModule']);
+        var myService = $injector.get('$localForage');
+        expect(myService.bind).toBeDefined();
+    });
+
+    it('service:unbind should be defined', function () {
+        var $injector = angular.injector(['LocalForageModule']);
+        var myService = $injector.get('$localForage');
+        expect(myService.unbind).toBeDefined();
+    });
+
+    it('directive should be defined', function () {
+        var $injector = angular.injector(['LocalForageModule']);
+    });
 });
