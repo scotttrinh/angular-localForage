f046da1cd811458e8b0cc1e8b1b5f4f981fb745b
mrobin <mrobin@companeo.com>
2014-02-20 16:50:36 +0100

    more asserts

--- tests/angular-localForage.js
+++ tests/angular-localForage.js
@@ -7,5 +7,15 @@ describe('Module: LocalForageModule', function () {
         var $injector = angular.injector(['LocalForageModule']);
         var myService = $injector.get('$localForage');
         expect(myService.setDriver).toBeDefined();
+        expect(myService.getDriver).toBeDefined();
+        expect(myService.set).toBeDefined();
+        expect(myService.get).toBeDefined();
+        expect(myService.remove).toBeDefined();
+        expect(myService.clearAll).toBeDefined();
+        expect(myService.getKeyAt).toBeDefined();
+        expect(myService.getKeys).toBeDefined();
+        expect(myService.getLength).toBeDefined();
+        expect(myService.bind).toBeDefined();
+        expect(myService.unbind).toBeDefined();
     });
 });
