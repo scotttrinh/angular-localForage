6665316781f84e5b000b59303c1f43cb52f5d419
mrobin <mrobin@companeo.com>
2014-02-20 17:06:27 +0100

    too much repeated asserts

--- tests/angular-localForage.js
+++ tests/angular-localForage.js
@@ -6,17 +6,7 @@ describe('Module: LocalForageModule', function () {
     it('service should be defined', function () {
         var $injector = angular.injector(['LocalForageModule']);
         var myService = $injector.get('$localForage');
-        expect(myService.setDriver).toBeDefined();
-        expect(myService.getDriver).toBeDefined();
-        expect(myService.set).toBeDefined();
-        expect(myService.get).toBeDefined();
-        expect(myService.remove).toBeDefined();
-        expect(myService.clearAll).toBeDefined();
-        expect(myService.getKeyAt).toBeDefined();
-        expect(myService.getKeys).toBeDefined();
-        expect(myService.getLength).toBeDefined();
-        expect(myService.bind).toBeDefined();
-        expect(myService.unbind).toBeDefined();
+        expect(myService).toBeDefined();
     });
 
     it('service:setDriver should be defined', function () {
