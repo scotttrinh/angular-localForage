fe38f97ec5f9aeffa9899eb2d218c97f670a3d83
Olivier Combe <olivier.combe@gmail.com>
2014-08-01 16:38:18 +0200

    chore: switch to gulp-rimraf instead of gulp-clean

--- gulpfile.js
+++ gulpfile.js
@@ -71,10 +71,10 @@ var makeChangelog = function(newVer) {
 		stream = streamqueue({ objectMode: true }),
 		exec = require('gulp-exec'),
 		concat = require('gulp-concat'),
-		clean = require('gulp-clean');
+		rimraf = require('gulp-rimraf');
 
 	stream.queue(gulp.src('').pipe(exec('node ./changelog.js ' + newVer, { pipeStdout: true })));
-	stream.queue(gulp.src('CHANGELOG.md').pipe(clean()));
+	stream.queue(gulp.src('CHANGELOG.md').pipe(rimraf()));
 
 	return stream.done()
 		.pipe(concat('CHANGELOG.md'))
