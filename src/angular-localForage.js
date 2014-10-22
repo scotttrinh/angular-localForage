49aa9fb4e899eea7f306d6478038d778c48ff18c
Denis Yaremov <denis.yaremov@gmail.com>
2014-10-23 01:15:02 +0300

    iterate support

--- src/angular-localForage.js
+++ src/angular-localForage.js
@@ -125,7 +125,27 @@
 				return deferred.promise;
 			};
 
-			// Remove an item from storage
+            // Directly get a value from storage
+            LocalForageInstance.prototype.iterate = function iterate(callback) {
+                // throw error on undefined key
+                if(angular.isUndefined(callback)) {
+                    throw new Error("You must define a callback to iterate");
+                }
+
+                var deferred = $q.defer(),
+                    args = arguments,
+                    self = this;
+
+                self._localforage.iterate(callback).then(function success(item) {
+                    deferred.resolve(item);
+                }, function error(data) {
+                    self.onError(data, args, self.iterate, deferred);
+                });
+
+                return deferred.promise;
+            };
+
+            // Remove an item from storage
 			LocalForageInstance.prototype.removeItem = function removeItem(key) {
 				// throw error on undefined key
 				if(angular.isUndefined(key)) {
