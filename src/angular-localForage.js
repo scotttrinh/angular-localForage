bcb80690cf7185ffa79dce4dfa15c8efbf326270
Gearóid Moroney <hello@gearoid.me>
2016-04-19 15:08:16 +0100

    fix: If there is no root, use angular from window

--- src/angular-localForage.js
+++ src/angular-localForage.js
@@ -1,7 +1,7 @@
 (function(root, factory) {
   'use strict';
 
-  var angular = root.angular || (window && window.angular);
+  var angular = root ? root.angular : window.angular;
   if(typeof define === 'function' && define.amd) {                    // AMD
     define(['localforage'], function(localforage) {
       factory(angular, localforage);
