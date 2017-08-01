1bd396e3f275b132f4c12efb2087c9efc7378a5b
Scott Trinh <strinh@Scotts-MacBook-Pro-2.local>
2017-08-01 00:56:06 -0400

    [WIP] Testing framework changes
    - Still need to get saucelabs up and running to test Internet Explorer, but in
      the meantime, this runs Firefox locally.
    - Eventually, we'll want to CI on:
      - Chrome
      - Firefox
      - Safari
      - Edge
      - IE8/9/10/11
    - Locally just run headless Chrome?

--- karma.conf.js
+++ karma.conf.js
@@ -1,100 +1,73 @@
-var customLaunchers = {
-  sl_chrome: {
-    base: 'SauceLabs',
-    browserName: 'chrome'
-  },
-  sl_firefox: {
-    base: 'SauceLabs',
-    browserName: 'firefox',
-    version: '30'
-  },
-  sl_safari: {
-    base: 'SauceLabs',
-    browserName: 'safari',
-    platform: 'OS X 10.9',
-    version: '7'
-  },
-  sl_ios_safari: {
-    base: 'SauceLabs',
-    browserName: 'iphone',
-    platform: 'OS X 10.9',
-    version: '7.1'
-  },
-  sl_ie_9: {
-    base: 'SauceLabs',
-    browserName: 'internet explorer',
-    platform: 'Windows 2008',
-    version: '9'
-  },
-  sl_ie_10: {
-    base: 'SauceLabs',
-    browserName: 'internet explorer',
-    platform: 'Windows 2012',
-    version: '10'
-  },
-  sl_ie_11: {
-    base: 'SauceLabs',
-    browserName: 'internet explorer',
-    platform: 'Windows 8.1',
-    version: '11'
-  }
-};
+// Karma configuration
+// Generated on Wed Apr 26 2017 09:14:28 GMT-0400 (EDT)
 
 module.exports = function(config) {
-  config.set(module.exports.conf);
+  config.set({
 
-  if(process.env.TRAVIS) {
-    config.sauceUser = process.env.SAUCE_USERNAME;
-    config.sauceKey = process.env.SAUCE_ACCESS_KEY;
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
 
-    var buildLabel = 'TRAVIS #' + process.env.TRAVIS_BUILD_NUMBER + ' (' + process.env.TRAVIS_BUILD_ID + ')';
 
-    config.logLevel = config.LOG_DEBUG;
-    config.transports = ['websocket', 'xhr-polling'];
-    config.captureTimeout = 0; // rely on SL timeout
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
 
-    config.sauceLabs.build = buildLabel;
-    config.sauceLabs.startConnect = false;
-    config.sauceLabs.tunnelIdentifier = process.env.TRAVIS_JOB_NUMBER;
-    config.sauceLabs.recordScreenshots = true;
 
-    // TODO(vojta): remove once SauceLabs supports websockets.
-    // This speeds up the capturing a bit, as browsers don't even try to use websocket.
-    config.transports = ['xhr-polling'];
-  }
-};
+    // list of files / patterns to load in the browser
+    files: [
+      './bower_components/angular/angular.js',
+      './bower_components/angular-mocks/angular-mocks.js',
+      './bower_components/localforage/dist/localforage.js',
+      './src/angular-localForage.js',
+      './tests/**/*.js',
+    ],
 
-module.exports.conf = {
-  frameworks: ['jasmine'],
-  files: [
-    'bower_components/angular/angular.js',
-    'bower_components/angular-mocks/angular-mocks.js',
-    'bower_components/localforage/dist/localforage.js',
-    'src/angular-localForage.js',
-    'tests/angular-localForage.js'
-  ],
-  autoWatch: false,
-  singleRun: true,
-  sauceLabs: {
-    testName: 'angular-localforage unit tests',
-    recordScreenshots: false,
-    startConnect: true,
-    options: {
-      'selenium-version': '2.41.0'
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
     },
-    connectOptions: {
-      port: 5757,
-      logfile: 'sauce_connect.log'
-    }
-  },
-  // Increase timeout in case connection in CI is slow
-  captureTimeout: 120000,
-  browserDisconnectTimeout: 10000,
-  browserDisconnectTolerance: 2,
-  browserNoActivityTimeout: 30000,
-  browsers: Object.keys(customLaunchers),
-  customLaunchers: customLaunchers,
-  reporters: ['progress', 'saucelabs'],
-  port: 9876,
-  colors: true
-};
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Firefox'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  });
+}
